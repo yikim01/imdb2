0c8d6359321105cc051d72ad262bdcae366ddfce
wdi loaner DTLA <dev@null.com>
2015-08-03 13:29:28 -0700

    update sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150427192437) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "actors", force: true do |t|
     t.text     "name"
     t.integer  "year_of_birth"
